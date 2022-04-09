55a5312a655467028eab2fc08c5180e398c4ea18
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2022-04-09 10:29:19 -0600

    Create .html, .css and .js files Add pseudocode as comments in .js Add brief description of project on README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+Rock, Paper, Sicssors game project for TOP.  Using vanilla JS for the initial part.  All of this is going on the console for now. 
---
+++ script.js
@@ -0,0 +1,7 @@
+// Generate rock, paper or scissors randomly for the computer.
+
+// Get input from player for rock, paper or scissors 
+// Convert player input to lowercase and trim spaces (this makes it not case sensitive)
+// Duel machine versus player, rock beats scissors, paper beats rock, scissors beat paper, same result is a draw
+// Count points from player and machine, a win is 1 point, a draw is 0.5 points, losing gives 0 points
+// Set match to last 5 rounds, at the end display who wins or if it is a draw

af97b954986b57051009daf6266bed97ddd58e7d
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2022-04-09 10:38:24 -0600

    Link html to js and create computerChoice function

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Generate rock, paper or scissors randomly for the computer.
+function computerPlay() {
+    // We choose a random number between 1 and 3
+    let randomNum = Math.floor((Math.random()* 3) + 1);
+    // We will store rock, paper or scissors here
+    let computerChoice; 
+    // Here we decide what number represents what choice for the game and assign it to computerChoice
+    switch (randomNum) {
+        case 1: 
+        computerChoice = 'rock';
+        break;
+        case 2: 
+        computerChoice = 'paper';
+        break;
+        case 3:
+        computerChoice = 'scissors'; 
+        break; 
+    }
+    return computerChoice;
+}
 
+computerPlay();
 // Get input from player for rock, paper or scissors 
 // Convert player input to lowercase and trim spaces (this makes it not case sensitive)
 // Duel machine versus player, rock beats scissors, paper beats rock, scissors beat paper, same result is a draw

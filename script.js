1ddceb7cfbaeb7c2a613a0d8fc4535b7688e3d97
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2022-04-19 20:20:29 -0600

    Add extra styles and coded 5 round match

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection;
+
 // Generate rock, paper or scissors randomly for the computer.
 function computerPlay() {
   // We choose a random number between 1 and 3
@@ -23,37 +27,49 @@ function computerPlay() {
 
 // Get input from player for rock, paper or scissors
 
-const playerBoard = document.getElementById('playerScore');
-const computerBoard = document.getElementById('computerScore');
-const resultBoard = document.getElementById('result');
-const resultPlayer = document.getElementById('player-result');
-const resultComputer = document.getElementById('computer-result');
-const buttons = document.querySelectorAll('button'); 
-let playerScore = 0;
-let computerScore = 0;
-let playerSelection;
-buttons.forEach(button => button.addEventListener('click', (e)=> {
-  playerSelection = e.target.id;
-  playRound();
-}));
+const playerBoard = document.getElementById("playerScore");
+const computerBoard = document.getElementById("computerScore");
+const resultBoard = document.getElementById("result");
+const resultPlayer = document.getElementById("player-result");
+const resultComputer = document.getElementById("computer-result");
+const buttons = document.querySelectorAll("button");
+const banner = document.querySelector("h2");
 
+console.log(document.getElementById("playerScore").textContent);
 
+playerBoard.textContent = playerScore;
+computerBoard.textContent = computerScore;
+resultBoard.textContent = "";
+banner.textContent = "Let's play!";
+banner.classList.remove("neonText");
+banner.classList.remove("computerNeonText");
 
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    playerSelection = e.target.id;
+    playRound();
+  })
+);
 
 // Duel machine versus player, rock beats scissors, paper beats rock, scissors beat paper, same result is a draw
 function playRound() {
+  banner.classList.remove("neonText");
+  banner.classList.remove("computerNeonText");
+  playerBoard.textContent = playerScore;
+  computerBoard.textContent = computerScore;
+  resultBoard.textContent = "";
+  banner.textContent = "Let's play!";
   // This will play one round of the game
   // We are storing the result of the round in this variable
   computerSelection = computerPlay();
 
- // We start by checking if a draw exists
+  // We start by checking if a draw exists
   if (computerSelection == playerSelection) {
-
     playerScore += 0.5;
     computerScore += 0.5;
     playerBoard.textContent = playerScore;
     computerBoard.textContent = computerScore;
-    resultBoard.textContent = 'It\'s a draw!';
+    resultBoard.textContent = "It's a draw!";
     resultPlayer.textContent = `You picked ${playerSelection}`;
     resultComputer.textContent = `The computer picked ${computerSelection}`;
   }
@@ -66,17 +82,48 @@ function playRound() {
     roundResult = "computer wins";
     computerScore += 1;
     computerBoard.textContent = computerScore;
-    resultBoard.textContent = 'Computer wins!';
+    resultBoard.textContent = "Computer wins!";
     resultPlayer.textContent = `You picked ${playerSelection}`;
     resultComputer.textContent = `The computer picked ${computerSelection}`;
   }
   // if none of those combinations happened, the player won
   else {
     roundResult = "player wins";
-    playerScore += 1
+    playerScore += 1;
     playerBoard.textContent = playerScore;
-    resultBoard.textContent = 'You win!';
+    resultBoard.textContent = "You win!";
     resultPlayer.textContent = `You picked ${playerSelection}`;
     resultComputer.textContent = `The computer picked ${computerSelection}`;
+  }
+  if (playerScore >= 5 && computerScore >= 5 && playerScore === computerScore) {
+    banner.classList.add("neonText");
+    banner.textContent = "The game ends in a draw!";
+    playerScore = 0;
+    computerScore = 0;
+    playerBoard.textContent = playerScore;
+    computerBoard.textContent = computerScore;
+    resultBoard.textContent = "";
+    resultPlayer.textContent = "";
+    resultComputer.textContent = "";
+  } else if (playerScore >= 5) {
+    banner.classList.add("neonText");
+    banner.textContent = "You win!!";
+    playerScore = 0;
+    computerScore = 0;
+    playerBoard.textContent = playerScore;
+    computerBoard.textContent = computerScore;
+    resultBoard.textContent = "";
+    resultPlayer.textContent = "";
+    resultComputer.textContent = "";
+  } else if (computerScore >= 5) {
+    banner.classList.add("computerNeonText");
+    banner.textContent = "Computer wins!!";
+    playerScore = 0;
+    computerScore = 0;
+    playerBoard.textContent = playerScore;
+    computerBoard.textContent = computerScore;
+    resultBoard.textContent = "";
+    resultPlayer.textContent = "";
+    resultComputer.textContent = "";
   }
 }
